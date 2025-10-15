dcf3c543c5b08614c13a2d4c0fee7e865bb35e0f
Chukwunonso Okpala <nonsosr@gmail.com>
2025-10-15 15:18:02 +0100

    chore: cleand up logic and output

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # GBP/JPY Converter
-# Will be developed later into something more advanced using
+# Will be developed later into something more advanced
 
 Amount = float(input("Enter an amount:"))
 currency = input("What currency is that? (GBP/JPY): ")
@@ -8,8 +8,10 @@ Amount = round(Amount, 2)
 if currency == "GBP":
     print(f"The amount £{Amount}")
     Amount = Amount * 202.56
-    print(f"Converted to: ¥",round(Amount, 2))
+    print(f"Converted to JPY is: ¥{round(Amount, 2):,.2f}")
 elif currency == "JPY":
-    print(f"The amount ¥{Amount}")
+    print(f"The amount ¥{Amount:,.2f}")
     Amount = Amount / 202.56
-    print("Converted to: £",round(Amount, 2))
+    print(f"Converted to GBP is: £{round(Amount, 2):,.2f}")
+else:
+    print(f"{currency} is not a valid currency.")
